ba90a08ffbe8f5bda6dd42d4b1f66c86936d02d2
harshmahalle <126699694+harshmahalle@users.noreply.github.com>
2023-10-16 20:59:22 +0530

    Add files via upload

---
+++ routes/authMiddleware.js
@@ -0,0 +1,14 @@
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
+module.exports = ensureAuthenticated;
+
+
+
+
+  
+
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-// Define a route for rendering the dashboard page
 router.get('/', (req, res) => {
-  res.render('dashboard'); // Render the "dashboard.ejs" view
+  // Your dashboard logic here
 });
 
 module.exports = router;
+
+
